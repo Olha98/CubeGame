a0cf98ddb6ff4ae8a8edc3c25a4ccab051e4aac9
Olha Hrymchak <forwork1177@gmail.com>
2020-11-13 22:55:27 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import img2 from './assets/images/2.png'
+import img3 from './assets/images/images.png'
+import img1 from './assets/images/images2.png'
+import styled from './App.module.css'
 
 
 function App() {
+  const [flippedCard, hasFlippedCard ] = useState(false)
+
+  const handelClisk = (e) => {
+  console.log(e.target)
+  }
+
+
   return (
-    <>
-    </>
-  //   <div class="memory-card" data-framework="react">
-  //   <img class="front-face" src="img/react.svg" alt="React">
-  //   <img class="back-face" src="img/js-badge.svg" alt="Memory Card">
-  // </div>
+    <section className={styled.memoryGame}>
+     <div className={styled.memoryCard} data-framework="react" onClick={(e) => handelClisk(e)}>
+    <img className={styled.frontFace} src={img3} alt="React"/>
+    <img className={styled.backFace} src={img2} alt="Memory Card"/>
+  </div>
+  <div className={styled.memoryCard} data-framework="angular" onClick={(e) => handelClisk(e)}>
+    <img className={styled.frontFace} src={img3} alt="React"/>
+    <img className={styled.backFace} src={img1} alt="Memory Card"/>
+  </div>
+   <div className={styled.memoryCard} data-framework="react" onClick={(e) => handelClisk(e)}>
+    <img className={styled.frontFace} src={img3} alt="React"/>
+    <img className= {styled.backFace} src={img2} alt="Memory Card"/>
+  </div>
+    </section>
+   
   );
 }
 
 export default App;
+
