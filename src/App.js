530f3e72452f28fcf695eea832b40ff13eadf8c7
Olha Hrymchak <forwork1177@gmail.com>
2020-11-13 21:38:10 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+    </>
+  //   <div class="memory-card" data-framework="react">
+  //   <img class="front-face" src="img/react.svg" alt="React">
+  //   <img class="back-face" src="img/js-badge.svg" alt="Memory Card">
+  // </div>
+  );
+}
+
+export default App;
